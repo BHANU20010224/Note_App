b14631a0451207b0cbd9e3fdee5ce41a9c49b050
Bhanu Ranaweera <bhanuranaweera24@gmail.com>
2025-06-08 12:42:34 -0700

    edit nav bar

--- frontend/NOTES-APP/src/components/Navbar/Navbar.jsx
+++ frontend/NOTES-APP/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Profileinfo from "../../components/Cards/Profileinfo";
 import { useNavigate } from "react-router-dom";
 import Searchbar from "../../components/Searchbar/Searchbar";
 const Navbar = () => {
 
+    const [searchQuery, setSearchQuery]= useState("") ;
+
     const navigate = useNavigate();
 
 const onLogout = () => {
     navigate("/signin");
-    }
+    };
+    const handleSearch = () => {};
+  
+    const onClearSearch = ()=>{
+    setSearchQuery ("");
+    };
+
+
 return (
   <>
 	<div className=" bg-white shadow-md p-4 flex justify-between items-center">
 	  <h2 className="text-xl font-medium text-black py-2 ustify-between items-center">Notes</h2>
-    <Searchbar />
-      <Profileinfo  onLogout={onLogout}/>
+
+<Searchbar
+  value={searchQuery}
+  onChange={({ target }) => setSearchQuery(target.value)}
+  
+  handleSearch={handleSearch}
+  onClearSearch={onClearSearch}
+/>
+<Profileinfo onLogout={onLogout}/>
 	</div>
 	
   </>
