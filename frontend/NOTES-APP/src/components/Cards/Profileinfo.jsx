dedc1bd1842f73b11dcbdcf97bf4af3c42e3c1d3
Bhanu Ranaweera <bhanuranaweera24@gmail.com>
2025-06-07 23:11:07 -0700

    change_profile_colour

--- frontend/NOTES-APP/src/components/Cards/Profileinfo.jsx
+++ frontend/NOTES-APP/src/components/Cards/Profileinfo.jsx
@@ -6,12 +6,12 @@ import { getInitials } from "../../Pages/Utils/helper";
 const ProfileInfo = ({ onLogout }) => {
   return (
 	<div className="flex items-center gap-3">
-	  <div className="w-12 h-12 flex items-center justify-center rounded-full tex">
-		{getInitials("bhanu Ranaweera")}
+	 <div className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-100">
+     {getInitials("bhanu Ranaweera")}
 	  </div>
 	  <div>
 		<p className="text-sm font-medium">Bhanu</p>
-		<button className="text-sm ☐text-slate-700 underline" onClick={onLogout}>
+		<button className="text-sm ☐text-slate-700 " onClick={onLogout}>
 		  Logout
 		</button>
 	  </div>
